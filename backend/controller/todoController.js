1ec732c61686ab00e23acfe9fb28f728f7152a37
Chisty-nightmare <maruhasan97@gmail.com>
2023-08-13 15:21:25 +0600

    Pagination for Search Api

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -68,6 +68,10 @@ const updateTodo = async (req,res) => {
 }
 const  searchByField = async (req,res) => {
 
+  let page= Number(req.query.page) || 1;
+  let limit=Number(req.query.limit) || 1;
+
+  let skip=(page-1)*(limit)
       let data = await Todo.find({
         "$or":
         [
@@ -83,7 +87,7 @@ const  searchByField = async (req,res) => {
 
         ]
 
-      })
+      }).skip(skip).limit(limit)
       return res.json(data)
 }
 
